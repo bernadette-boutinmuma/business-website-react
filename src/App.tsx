f567c65951ddab5064d8f834de332b973b8d4784
zhou fang <linkfang@outlook.com>
2023-08-19 12:36:45 -0400

    Finished basic nav set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,138 @@
 import "./App.css";
+import Nav from "./components/nav/Nav";
 
 function App() {
   return (
     <>
-      <nav>
-        <a href="https://vitejs.dev" target="_blank">
-          About us
-        </a>
-        <a href="https://react.dev" target="_blank">
-          Services
-        </a>
-      </nav>
-      <h1>Pesticide Product Chemistry Regulatory Consulting</h1>
+      <Nav />
+      {/* <div style={{ height: 64 }}></div> */}
+      <h2 id="our-journey">PChem Consulting Inc.'s Journey</h2>
 
       <p>
-        I am an analytical chemist by training, having significant knowledge of
-        the pesticide regulatory process in Canada, and of the Global joint
-        review process, with emphasis on product chemistry.{" "}
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
       </p>
       <p>
-        I worked as a pesticide residue chemist before joining Health Canada's
-        Pest Management Regulatory Agency (PMRA). I evaluated chemistry data for
-        all types of submissions for over 15 years as a senior scientific
-        evaluator, and for two years I led the chemistry unit and the Formulants
-        section, as a section head.
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
       </p>
       <p>
-        I have helped companies prepare product chemistry packages to regulatory
-        agencies to fulfill government requirements for pesticide product
-        registration in Canada, the US and internationally. I am fully bilingual
-        and can assist with French translation. I am a professional who is
-        dedicated to providing a valued regulatory consulting service with
-        respect to product chemistry.
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+
+      <h2 id="our-mission">Our Mission</h2>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+
+      <h2 id="what-we-do">What We Do</h2>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+
+      <h2 id="our-services">Our Services</h2>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
+      </p>
+      <p>
+        I am an analytical chemist by training, having significant knowledge of the pesticide regulatory process in
+        Canada, and of the Global joint review process, with emphasis on product chemistry.{" "}
+      </p>
+      <p>
+        I worked as a pesticide residue chemist before joining Health Canada's Pest Management Regulatory Agency (PMRA).
+        I evaluated chemistry data for all types of submissions for over 15 years as a senior scientific evaluator, and
+        for two years I led the chemistry unit and the Formulants section, as a section head.
+      </p>
+      <p>
+        I have helped companies prepare product chemistry packages to regulatory agencies to fulfill government
+        requirements for pesticide product registration in Canada, the US and internationally. I am fully bilingual and
+        can assist with French translation. I am a professional who is dedicated to providing a valued regulatory
+        consulting service with respect to product chemistry.
       </p>
     </>
   );
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,28 @@
+const Nav = () => {
+  return (
+    <div style={{ backgroundColor:'#fff', boxShadow: "0 0 16px rgba(0,0,0,0.1)", position: "fixed", inset: "0 0 0 0", bottom: 'unset' }}>
+      <div
+        style={{
+          maxWidth: 1280,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "16px 0",
+          margin: "0 auto",
+        }}
+      >
+        <img src="./logo-s.png" alt="PChem Regulatory Consultant logo" width={150} />
+        <nav style={{ display: "flex", justifyContent: "center", gap: 32 }}>
+          <a href="#our-journey">Our Journey</a>
+          <a href="#our-mission">Our Mission</a>
+          <a href="#what-we-do">What We Do</a>
+          <a href="#our-services">Our Services</a>
+          <a href="#location">Location</a>
+          <a href="#contact-us">Contact Us</a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
