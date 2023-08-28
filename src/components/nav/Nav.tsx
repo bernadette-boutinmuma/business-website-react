51e7cf66c4461e950281800f50643d99b5b411dd
zhou fang <linkfang@outlook.com>
2023-08-27 22:24:56 -0400

    Added hamburger menu

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,15 @@
+import { MenuOutlined } from "@ant-design/icons"
+import { COLORS } from "../../constants/general"
+import { useState } from "react"
+
+const anchorStyle = {
+  borderBottom: `2px ${COLORS.primary}25 solid`,
+  padding: "16px 24px",
+  textAlign: "center",
+} as const
+
 const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <div
       style={{
@@ -8,6 +19,7 @@ const Nav = () => {
         position: "fixed",
         inset: "0 0 0 0",
         bottom: "unset",
+        height: 63.5,
       }}
     >
       <div
@@ -17,13 +29,13 @@ const Nav = () => {
           justifyContent: "space-between",
           alignItems: "center",
           padding: "8px 48px",
-          margin: "0 auto",
+          margin: " 0 auto",
         }}
       >
         <a href="#">
           <img src="./logo-s.png" alt="PChem Regulatory Consultant logo" height={42} width={99.5} />
         </a>
-        <nav style={{ display: "flex", justifyContent: "center", gap: 32 }}>
+        <nav className="navCtn">
           <a href="#our-journey">Our Journey</a>
           <a href="#our-mission">Our Mission</a>
           <a href="#what-we-do">What We Do</a>
@@ -31,6 +43,53 @@ const Nav = () => {
           <a href="#location">Location</a>
           <a href="#contact-us">Contact Us</a>
         </nav>
+
+        <div
+          style={{
+            display: showMenu ? "block" : "none",
+            position: "fixed",
+            inset: 0,
+            backgroundColor: "#000",
+            opacity: 0.6,
+            cursor: "pointer",
+          }}
+          onClick={() => setShowMenu(false)}
+        ></div>
+
+        <nav
+          style={{
+            display: showMenu ? "flex" : "none",
+            flexDirection: "column",
+            position: "fixed",
+            inset: 48,
+            backgroundColor: "#fff",
+            borderRadius: 8,
+          }}
+          onClick={() => setShowMenu(false)}
+        >
+          <a style={anchorStyle} href="#our-journey">
+            Our Journey
+          </a>
+          <a style={anchorStyle} href="#our-mission">
+            Our Mission
+          </a>
+          <a style={anchorStyle} href="#what-we-do">
+            What We Do
+          </a>
+          <a style={anchorStyle} href="#our-services">
+            Our Services
+          </a>
+          <a style={anchorStyle} href="#location">
+            Location
+          </a>
+          <a style={anchorStyle} href="#contact-us">
+            Contact Us
+          </a>
+        </nav>
+
+        <div className="hamburgerIcon" onClick={() => setShowMenu((pre) => !pre)}>
+          <MenuOutlined style={{ color: COLORS.primary, fontSize: 22, cursor: "pointer" }} />
+        </div>
       </div>
     </div>
   )
