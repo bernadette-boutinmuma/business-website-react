518ab4c883668a605e173202eebf3012607016cf
zhou fang <linkfang@outlook.com>
2023-10-01 14:49:44 -0400

    updated google map location

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -7,10 +7,10 @@ const Location = () => {
         <h2 style={{ marginTop: 0 }}>Location</h2>
 
         <iframe
-          title="location-google-map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2801.211495205061!2d-75.7249315438753!3d45.405074563032265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce043867eceb47%3A0x8c01bf3056fbd6ac!2s72%20Bayswater%20Ave%2C%20Ottawa%2C%20ON%20K1Y%202E7!5e0!3m2!1sen!2sca!4v1692543873675!5m2!1sen!2sca"
+          title="pchem-consulting-inc-location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325.5380676534473!2d-75.72019518991537!3d45.40500427995148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05706d8c8bff%3A0xaad794f5d0b9b362!2sPChem%20Consulting%20Inc!5e0!3m2!1sen!2sca!4v1696185726452!5m2!1sen!2sca"
           style={{ width: "100%", height: 400, border: 0, borderRadius: 8 }}
-          loading="lazy"
+          loading="eager"
           referrerPolicy="no-referrer-when-downgrade"
         />
       </div>
